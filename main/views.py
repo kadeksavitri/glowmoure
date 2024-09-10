c059779abec3c4929040552fe592675628dddac5
kadeksavitri <kadeksavitri5@gmail.com>
2024-09-10 17:17:32 +0700

    done edit pws

--- main/views.py
+++ main/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name' : 'Glowmoure',
-        'price' : '30000',
-        'description' : 'Glowmoure.id offers aesthetic scented candles with delightful fragrances and cute acrylic nightlights that add a cozy glow to any space. Bring charm and warmth to your home with our beautifully designed products!',
-        'category' : 'scented candle',
-        'stock' : '10'
-
+        'name': 'Kadek Savitri',
+        'npm' : '2306203236'
+        
     }
 
     return render(request, "main.html", context)
